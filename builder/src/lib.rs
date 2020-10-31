fbc8c6b4b17e02b6e718fe8ea7712e91f7ffa015
Tommaso <p.tommy93@gmail.com>
2020-10-31 18:56:40 +0100

    second test working

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,34 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    println!("input: {:#?}", input);
+    let struct_name = input.ident;
+    let internal_struct = format_ident!("{}Builder", struct_name);
 
-    TokenStream::new()
+    let expanded = quote! {
+        impl #struct_name {
+            pub fn builder() -> #internal_struct {
+                #internal_struct{
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+
+        pub struct #internal_struct {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        };
+    };
+
+    proc_macro::TokenStream::from(expanded)
 }
