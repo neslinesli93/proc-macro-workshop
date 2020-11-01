93564de409bd83d6ae74d39e2ba0b071096ac8ec
Tommaso <p.tommy93@gmail.com>
2020-11-01 10:07:52 +0100

    third test working

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -22,6 +22,28 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             }
         }
 
+        impl #internal_struct {
+            fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+
+            fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+
+            fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+
+            fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+        }
+
         pub struct #internal_struct {
             executable: Option<String>,
             args: Option<Vec<String>>,
