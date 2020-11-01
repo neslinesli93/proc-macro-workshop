891d3ad8ff7d818fb7084a89f9a7a2b61a36a749
Tommaso <p.tommy93@gmail.com>
2020-11-01 15:29:29 +0100

    WIP: sixth test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,18 @@
 extern crate proc_macro;
 
+use proc_macro_error::abort_call_site;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed, Type, TypePath};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
+    let fields = get_fields(input.clone());
+    let get_current_dir = gen_getter_string(fields, "current_dir".to_string());
+
     let struct_name = input.ident;
+
     let internal_struct = format_ident!("{}Builder", struct_name);
 
     let expanded = quote! {
@@ -23,6 +28,8 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
 
         impl #internal_struct {
+            #get_current_dir
+
             fn executable(&mut self, executable: String) -> &mut Self {
                 self.executable = Some(executable);
                 self
@@ -48,6 +55,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
                 #struct_name::try_from(self).map_err(|error| error.into())
             }
+
         }
 
         impl std::convert::TryFrom<&mut #internal_struct> for #struct_name {
@@ -57,7 +65,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 let executable = value.executable.to_owned().ok_or_else(|| "Missing field: 'executable'".to_string())?;
                 let args = value.args.to_owned().ok_or_else(|| "Missing field: 'args'".to_string())?;
                 let env = value.env.to_owned().ok_or_else(|| "Missing field: 'env'".to_string())?;
-                let current_dir = value.current_dir.to_owned().ok_or_else(|| "Missing field: 'current_dir'".to_string())?;
+                let current_dir = #internal_struct::get_current_dir(value)?;
 
                 Ok(#struct_name {
                     executable,
@@ -77,4 +85,60 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     };
 
     proc_macro::TokenStream::from(expanded)
+}
+
+fn get_fields(input: DeriveInput) -> FieldsNamed {
+    match input.data {
+        Data::Struct(DataStruct { fields, .. }) => match fields {
+            Fields::Named(fields) => fields,
+            _ => abort_call_site!("only named fields are supported"),
+        },
+        _ => abort_call_site!("only structs are supported"),
+    }
+}
+
+fn gen_getter_string(fields: FieldsNamed, ident: String) -> proc_macro2::TokenStream {
+    let getter = format_ident!("get_{}", ident);
+
+    let field = fields.named.into_iter().find(|f| match &f.ident {
+        Some(i) => i.to_string() == ident,
+        _ => false,
+    });
+
+    if field.is_none() {
+        let result = quote! {
+            pub fn #getter(&mut self) -> Result<Option<String>, String> { Ok(None) }
+        };
+
+        return result.into();
+    }
+
+    let field_ident = field.clone().unwrap().ident.unwrap();
+
+    let optional: bool = field
+        .map(|f| match f.ty {
+            Type::Path(TypePath { path, .. }) => !path
+                .segments
+                .into_iter()
+                .any(|s| s.ident.to_string() == "Option".to_string()),
+            _ => true,
+        })
+        .unwrap_or_else(|| true);
+
+    let result = if optional {
+        quote! {
+            pub fn #getter(&mut self) -> Result<Option<String>, String> {
+                Ok(self.#field_ident)
+            }
+        }
+    } else {
+        let msg = format!("Missing field: {}", ident);
+        quote! {
+            pub fn #getter(&mut self) -> Result<Option<String>, String> {
+                self.#field_ident.ok_or_else(|| #msg)
+            }
+        }
+    };
+
+    result.into()
 }
